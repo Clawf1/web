e15ba0c50c38ae9441e519d2202465a133f741c5
Denis <clawfight@mail.ru>
2024-11-22 18:58:08 +0300

    lab0

--- mini-app/src/panels/Home.tsx
+++ mini-app/src/panels/Home.tsx
@@ -10,7 +10,7 @@ import {
   Avatar,
   NavIdProps,
 } from '@vkontakte/vkui';
-import { UserInfo } from '@vkontakte/vk-bridge';
+import bridge, { UserInfo } from '@vkontakte/vk-bridge';
 import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
 export interface HomeProps extends NavIdProps {
@@ -20,6 +20,19 @@ export interface HomeProps extends NavIdProps {
 export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
   const { photo_200, city, first_name, last_name } = { ...fetchedUser };
   const routeNavigator = useRouteNavigator();
+
+  const getRandomImageUrl = () => {
+    return 'https://cataas.com/cat?width=450&height=800';
+  };
+
+  const openStoryEditor = async () => {
+    const imageUrl = getRandomImageUrl();
+
+    await bridge.send('VKWebAppShowStoryBox', {
+      background_type: 'image',
+      url: imageUrl,
+    });
+  };
 
   return (
     <Panel id={id}>
@@ -36,6 +49,14 @@ export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
         <Div>
           <Button stretched size="l" mode="secondary" onClick={() => routeNavigator.push('persik')}>
             Покажите Персика, пожалуйста!
+          </Button>
+        </Div>
+      </Group>
+
+      <Group header={<Header mode="secondary">Редактор Историй</Header>}>
+        <Div>
+          <Button stretched size="l" mode="primary" onClick={openStoryEditor}>
+            Открыть редактор историй
           </Button>
         </Div>
       </Group>
